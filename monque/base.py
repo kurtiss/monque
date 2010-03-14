c37f26d61a3592c8e894b777eb68372d0940e9d0
Kurtiss Hare <kurtiss@gmail.com>
2010-03-14 15:30:57 -0700

    Combines delayed and immediate queues into a single collection.  Tabs -> Spaces.  Ensures appropriate collection indexes for dequeueing.

--- monque/base.py
+++ monque/base.py
@@ -6,6 +6,7 @@ base.py
 Created by Kurtiss Hare on 2010-03-12.
 """
 
+import base64
 import datetime
 import functools
 import job
@@ -13,81 +14,84 @@ import operator
 import pymongo
 import pymongo.errors
 import pymongo.son
+import uuid
 
 
 class Monque(object):
-	def __init__(self, mongodb, collection_prefix = "monque", default_queue = 'default_q', max_retries = 5):
-		self.mongodb = mongodb
-		self._collection_prefix = collection_prefix
-		self._job_configuration = dict(
-			queue	= default_queue,
-			retries	= max_retries,
-			delay	= datetime.timedelta()
-		)
-	
-	def enqueue(self, job, **kwargs):
-		job = job.__configure__(kwargs)
-		c = self._get_job_collection(job)
-		now = datetime.datetime.utcnow()
+    def __init__(self, mongodb, collection_prefix = "monque", default_queue = 'default_queue', max_retries = 5):
+        self.mongodb = mongodb
+        self._collection_prefix = collection_prefix
+        self._job_configuration = dict(
+            queue   = default_queue,
+            retries = max_retries,
+            delay   = datetime.timedelta()
+        )
+        self._initialized_queues = dict()
+    
+    def enqueue(self, job, **kwargs):
+        job = job.__configure__(kwargs)
+        c = self._get_job_collection(job)
+        now = datetime.datetime.utcnow()
 
-		if job.delay:
-			c = self._get_delayed_queue_collection(job.queue)
-			c.insert(dict(
-				inserted_time	= now,
-				scheduled_time	= now + job.delay,
-				queue			= job.queue,
-				job				= job.__serialize__()
-			))
-		else:
-			c = self.get_queue_collection(job.queue)
-			c.insert(dict(
-				inserted_time	= now,
-				queue			= job.queue,
-				job				= job.__serialize__()
-			))
+        c = self._get_queue_collection(job.queue)
+        c.insert(dict(
+            inserted_time   = now,
+            scheduled_time  = now + job.delay,
+            random_token    = _random_token(),
+            queue           = job.queue,
+            message         = job.__serialize__()
+        ))
 
-	def dequeue(self, queues):
-		for queue in queues:
-			job = self._dequeue_from(queue)
+    def dequeue(self, queues):
+        for queue in queues:
+            result = self._dequeue_from(queue)
 
-			if job:
-				return job
-	
-	def _dequeue_from(self, queue):
-		c = self.get_queue_collection(queue)
-		dc = self._get_delayed_queue_collection(queue)
-		now = datetime.datetime.utcnow()
-		result = None
+            if result:
+                return result
+    
+    def _dequeue_from(self, queue):
+        c = self.get_queue_collection(queue)
+        now = datetime.datetime.utcnow()
+        capture_token = _random_token()
+        directions = (('$gte', pymongo.ASCENDING), ('$lt', pymongo.DESCENDING))
 
-		delayed_messages = dc.find(dict(
-			scheduled_time = { '$lte' : now }
-		), limit = 10).sort('scheduled_time', pymongo.ASCENDING)
-		
-		for delayed_message in delayed_messages:
-			try:
-				dc.remove(delayed_job._id, safe = True)
-			except pymongo.errors(pymongo.errors.OperationFailure):
-				pass
-			else:
-				delayed_message.pop('_id', None)
-				delayed_message.pop('scheduled_time', None)
-				c.insert(delayed_message)
-		
-		message = self.mongodb.command(pymongo.son.SON([
-			('findandmodify', c.name),
-			('remove', True)
-		]))
+        for operator, order in directions:
+            message = self.mongodb.command(pymongo.son.SON([
+                ('findandmodify', c.name),
+                ('query', dict(
+                    scheduled_time  = {'$lte' : now},
+                    random_token    = { random_operator : capture_token }
+                )),
+                ('sort', dict(random_token = order)),
+                ('remove', True)
+            ]))
 
-		if message:
-			result = job.MonqueJob.__deserialize__(message)
-		
-		return result
+            if message:
+                return job.MonqueJob.__deserialize__(message)
+    
+    def _initialize_queue(coll):
+        coll.ensure_index([
+            ('scheduled_time',  pymongo.ASCENDING),
+            ('random_token',    pymongo.ASCENDING)
+        ])
+        
+        coll.ensure_index([
+            ('scheduled_time',  pymongo.ASCENDING),
+            ('random_token',    pymongo.DESCENDING)
+        ])
 
-	def get_collection(self, *args):
-		return operator.getitem(self.mongodb, ':'.join([self._collection_prefix] + args))
+    def get_collection(self, *args):
+        return operator.getitem(self.mongodb, ':'.join([self._collection_prefix] + args))
 
-	def get_queue_collection(self, queue):
-		return self.get_collection('queues', queue)
+    def get_queue_collection(self, queue):
+        coll = self.get_collection('queues', queue)
 
-	def _get_delayed_queue_collection(self, queue):
-		return self.get_collection('queues', queue, 'delayed')
+        if not self._initialized_queues.has_key(coll.name):
+            self._initialize_queue(coll)
+            self._initialized_queues[coll.name] = True
+
+        return coll
+
+        
+def _random_token():
+    return base64.b64encode(uuid.uuid4().bytes, ('-', '_')).rstrip('=')        
--- monque/util.py
+++ monque/util.py
@@ -9,35 +9,39 @@ Created by Kurtiss Hare on 2010-03-12.
 _import_cache = dict()
 _setprocname = None
 
-def full_import(name):
-	global _import_cache
 
-	if not _import_cache.has_key(name):
-		mod_name, attr_name = name.rsplit('.', 1)
-		mod = __import__(str(mod_name), {}, {}, [str(attr_name)])
-		attr = getattr(mod, attr_name)
-		self._import_cache[name] = attr
+def get_toplevel_attrname(obj):
+    return "{0.__module__}.{0.__name__}".format(obj)
 
-	return self._import_cache[name]
+def get_toplevel_attr(name):
+    global _import_cache
+
+    if not _import_cache.has_key(name):
+        mod_name, attr_name = name.rsplit('.', 1)
+        mod = __import__(str(mod_name), {}, {}, [str(attr_name)])
+        attr = getattr(mod, attr_name)
+        self._import_cache[name] = attr
+
+    return self._import_cache[name]
 
 def setprocname(name):
-	global _setprocname
+    global _setprocname
 
-	if _setprocname is None:
-		try:
-			import procname
-			_setprocname = procname.setprocname
-		except ImportError:
-			try:
-				from ctypes import byref, cdll, create_string_buffer
-				libc = cdll.LoadLibrary('libc.so.6')
+    if _setprocname is None:
+        try:
+            import procname
+            _setprocname = procname.setprocname
+        except ImportError:
+            try:
+                from ctypes import byref, cdll, create_string_buffer
+                libc = cdll.LoadLibrary('libc.so.6')
 
-				def _setprocname(name):
-					buff = create_string_buffer(len(name) + 1)
-					buff.value = name
-					libc.prctl(15, byref(buff), 0, 0, 0)
-			except (ImportError, OSError):
-				def _setprocname(name):
-					pass
+                def _setprocname(name):
+                    buff = create_string_buffer(len(name) + 1)
+                    buff.value = name
+                    libc.prctl(15, byref(buff), 0, 0, 0)
+            except (ImportError, OSError):
+                def _setprocname(name):
+                    pass
 
-	return _setprocname(name)
+    return _setprocname(name)
